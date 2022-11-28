1a25ce83d86afb26fb5b5b5ae0fd8c38759d37d9
kadu-v <kazuonsunday@yahoo.co.jp>
2022-11-28 17:30:52 +0000

    initila deploy

--- src/components/SidebarIcon.tsx
+++ src/components/SidebarIcon.tsx
@@ -3,6 +3,7 @@ import "./SidebarIcon.css";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import React from "react";
 import Link from "@mui/material/Link";
 
@@ -11,6 +12,11 @@ const SidebarIcon: React.FC<{}> = () => {
     <div className="SidebarIcon">
       <img src={Icon} alt="kadu-v" />
       <div className="SNSIcons">
+        {/* <div className="SNSIcon">
+          <Link href="x" color="inherit">
+            <LinkedInIcon fontSize="large" />
+          </Link>
+        </div> */}
         <div className="SNSIcon">
           <Link href="mailto:xxx" color="inherit">
             <EmailIcon fontSize="large" />
